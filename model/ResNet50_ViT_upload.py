b2ea8733becfab0cd451f217fc0520e769a76a20
Gabrellaaa <635352060@qq.com>
2023-03-01 22:05:28 +0800

    Add files via upload

---
+++ model/ResNet50_ViT_upload.py
@@ -0,0 +1,402 @@
+import torch
+from torch import nn
+
+from einops import rearrange, repeat
+from einops.layers.torch import Rearrange
+
+
+def pair(t):
+    return t if isinstance(t, tuple) else (t, t)
+
+def conv3x3(in_planes, out_planes, stride=1, groups=1, dilation=1):
+    """3x3 convolution with padding"""
+    return nn.Conv2d(in_planes, out_planes, kernel_size=3, stride=stride,
+                     padding=dilation, groups=groups, bias=False, dilation=dilation)
+
+def conv1x1(in_planes, out_planes, stride=1):
+    """1x1 convolution"""
+    return nn.Conv2d(in_planes, out_planes, kernel_size=1, stride=stride, bias=False)
+
+
+class PreNorm(nn.Module):
+    def __init__(self, dim, fn):
+        super().__init__()
+        self.norm = nn.LayerNorm(dim)
+        self.fn = fn
+    def forward(self, x, **kwargs):
+        return self.fn(self.norm(x), **kwargs)
+
+class FeedForward(nn.Module):
+    def __init__(self, dim, hidden_dim, dropout = 0.):
+        super().__init__()
+        self.net = nn.Sequential(
+            nn.Linear(dim, hidden_dim),
+            nn.GELU(),
+            nn.Dropout(dropout),
+            nn.Linear(hidden_dim, dim),
+            nn.Dropout(dropout)
+        )
+    def forward(self, x):
+        return self.net(x)
+
+class Attention(nn.Module):
+    def __init__(self, dim, heads = 8, dim_head = 64, dropout = 0.):
+        super().__init__()
+        inner_dim = dim_head *  heads
+        project_out = not (heads == 1 and dim_head == dim)
+
+        self.heads = heads
+        self.scale = dim_head ** -0.5
+
+        self.attend = nn.Softmax(dim = -1)
+        self.to_qkv = nn.Linear(dim, inner_dim * 3, bias = False)
+
+        self.to_out = nn.Sequential(
+            nn.Linear(inner_dim, dim),
+            nn.Dropout(dropout)
+        ) if project_out else nn.Identity()
+
+    def forward(self, x):
+        qkv = self.to_qkv(x).chunk(3, dim = -1)
+        # head多头
+        q, k, v = map(lambda t: rearrange(t, 'b n (h d) -> b h n d', h = self.heads), qkv)
+
+        dots = torch.matmul(q, k.transpose(-1, -2)) * self.scale
+
+        attn = self.attend(dots)
+        out = torch.matmul(attn, v)
+
+        out = rearrange(out, 'b h n d -> b n (h d)')
+        out = self.to_out(out)
+        return out
+
+class Transformer(nn.Module):
+    def __init__(self, dim, depth, heads, dim_head, mlp_dim, dropout = 0.):
+        super().__init__()
+        self.layers = nn.ModuleList([])
+        for _ in range(depth):
+            self.layers.append(nn.ModuleList([
+                PreNorm(dim, Attention(dim, heads = heads, dim_head = dim_head, dropout = dropout)),
+                PreNorm(dim, FeedForward(dim, mlp_dim, dropout = dropout))
+            ]))
+    def forward(self, x):
+        for attn, ff in self.layers:
+            x = attn(x) + x
+            x = ff(x) + x
+        return x
+
+
+class ViT(nn.Module):
+    def __init__(self, *, image_size, dim, patch_size, depth, heads, mlp_dim, channels,dim_head=64,dropout=0.,emb_dropout=0.):
+        super().__init__()
+        image_height, image_width = pair(image_size)
+        patch_height, patch_width = pair(patch_size)
+        assert image_height % patch_height == 0 and image_width % patch_width == 0, 'Image dimensions must be divisible by the patch size.'
+        num_patche = image_height // patch_height
+        num_patches = (image_height // patch_height) * (image_width // patch_width)
+        reduction_ratio = 32
+        patch_dim = patch_height * patch_width * (channels//reduction_ratio)
+        # 这个reduction ratio可以好好调一下，可以增加蛮多的。
+        # r=32  53.67
+        # r=16 57.72
+        self.to_patch_embedding = nn.Sequential(
+            nn.Conv2d(channels, channels//8, kernel_size=3, stride=1, padding=1),
+            # nn.Conv2d(channels//8, channels//32, kernel_size=1),
+            nn.Conv2d(channels//8, channels//reduction_ratio, kernel_size=1),
+            Rearrange('b c (h p1) (w p2) -> b (h w) (p1 p2 c)', p1 = patch_height, p2 = patch_width),
+            nn.Linear(patch_dim, dim),
+        )
+        # 位置编码，可学习的一维向量
+        self.pos_embedding = nn.Parameter(torch.randn(1, num_patches, dim))
+        # 分类token
+        self.cls_token = nn.Parameter(torch.randn(1, 1, dim))
+        self.dropout = nn.Dropout(emb_dropout)
+        self.transformer = Transformer(dim, depth, heads, dim_head, mlp_dim, dropout)
+        self.to_latent = nn.Identity()
+        self.mlp_head = nn.Sequential(
+            nn.LayerNorm(dim),
+            nn.Linear(dim, patch_dim),
+        )
+        self.re = Rearrange(' b (h w) (p1 p2 c) -> b c (h p1) (w p2)', h = num_patche, w = num_patche, p1 = patch_height, p2 = patch_width)
+        # self.reConv = nn.Conv2d(channels//32, channels, kernel_size=1)
+
+    def forward(self, img):
+        x = self.to_patch_embedding(img)
+        b, n, _ = x.shape
+        x += self.pos_embedding[:, :n]
+        x = self.dropout(x)
+        x = self.transformer(x)
+
+        x = self.to_latent(x)
+        x = self.mlp_head(x)
+        x = self.re(x)
+        x = torch.mean(x, dim=1, keepdim=True).expand_as(img)
+        return x
+
+class ViT_reduction(nn.Module):
+    def __init__(self, *, image_size, dim, patch_size, depth, heads, mlp_dim, channels,dim_head=64,dropout=0.,emb_dropout=0.):
+        super().__init__()
+        image_height, image_width = pair(image_size)
+        patch_height, patch_width = pair(patch_size)
+        assert image_height % patch_height == 0 and image_width % patch_width == 0, 'Image dimensions must be divisible by the patch size.'
+        num_patche = image_height // patch_height
+        num_patches = (image_height // patch_height) * (image_width // patch_width)
+        reduction_ratio = 32
+        patch_dim = patch_height * patch_width * (channels//reduction_ratio)
+        self.to_patch_embedding = nn.Sequential(
+            nn.Conv2d(channels, channels//8, kernel_size=3, stride=1, padding=1),
+            # nn.Conv2d(channels//8, channels//32, kernel_size=1),
+            nn.Conv2d(channels//8, channels//reduction_ratio, kernel_size=1),
+            Rearrange('b c (h p1) (w p2) -> b (h w) (p1 p2 c)', p1 = patch_height, p2 = patch_width),
+            nn.Linear(patch_dim, dim),
+        )
+        
+        self.pos_embedding = nn.Parameter(torch.randn(1, num_patches, dim))
+        
+        self.cls_token = nn.Parameter(torch.randn(1, 1, dim))
+        self.dropout = nn.Dropout(emb_dropout)
+        self.transformer = Transformer(dim, depth, heads, dim_head, mlp_dim, dropout)
+        self.to_latent = nn.Identity()
+        self.mlp_head = nn.Sequential(
+            nn.LayerNorm(dim),
+            nn.Linear(dim, patch_dim),
+        )
+        self.re = Rearrange(' b (h w) (p1 p2 c) -> b c (h p1) (w p2)', h = num_patche, w = num_patche, p1 = patch_height, p2 = patch_width)
+        # self.reConv = nn.Conv2d(channels//reduction_ratio, channels, kernel_size=1)
+
+        self.Classifier_Linear = nn.Linear(dim, 7)
+
+    def forward(self, img):
+        x = self.to_patch_embedding(img)
+        b, n, _ = x.shape
+        x += self.pos_embedding[:, :n]
+        x = self.dropout(x)
+        x = self.transformer(x)
+
+        '''information entropy'''
+        y = self.Classifier_Linear(x)
+        
+        information = torch.softmax(y, -1)    
+        entropy = -torch.sum(information * torch.log(information), dim=-1, keepdim=True)/2.0
+        ones = torch.ones_like(entropy)
+        # 0.8=90.00
+        mask = torch.where(entropy <= 0.75, ones, entropy).expand_as(x)
+        zeros = torch.zeros_like(mask)
+        mask = torch.where(entropy > 0.75, zeros, mask)
+        x = x * mask
+
+        x = self.to_latent(x)
+        x = self.mlp_head(x)
+        x = self.re(x)
+        # x = self.reConv(x)
+        x = torch.mean(x, dim=1, keepdim=True).expand_as(img)
+        y = torch.mean(y, dim=1)
+        return x,y
+
+
+class BasicBlock(nn.Module):
+    expansion = 1
+
+    def __init__(self, inplanes, planes, stride=1, downsample=None, groups=1,
+                 base_width=64, dilation=1, norm_layer=None):
+        super(BasicBlock, self).__init__()
+        if norm_layer is None:
+            norm_layer = nn.BatchNorm2d
+        if groups != 1 or base_width != 64:
+            raise ValueError('BasicBlock only supports groups=1 and base_width=64')
+        if dilation > 1:
+            raise NotImplementedError("Dilation > 1 not supported in BasicBlock")
+        # Both self.conv1 and self.downsample layers downsample the input when stride != 1
+        self.conv1 = conv3x3(inplanes, planes, stride)
+        self.bn1 = norm_layer(planes)
+        self.relu = nn.ReLU(inplace=True)
+        self.conv2 = conv3x3(planes, planes)
+        self.bn2 = norm_layer(planes)
+        self.downsample = downsample
+        self.stride = stride
+
+    def forward(self, x):
+        identity = x
+
+        out = self.conv1(x)
+        out = self.bn1(out)
+        out = self.relu(out)
+
+        out = self.conv2(out)
+        out = self.bn2(out)
+
+        if self.downsample is not None:
+            identity = self.downsample(x)
+
+        out += identity
+        out = self.relu(out)
+
+        return out
+
+
+class Bottleneck(nn.Module):
+    # Bottleneck in torchvision places the stride for downsampling at 3x3 convolution(self.conv2)
+    # while original implementation places the stride at the first 1x1 convolution(self.conv1)
+    # according to "Deep residual learning for image recognition"https://arxiv.org/abs/1512.03385.
+    # This variant is also known as ResNet V1.5 and improves accuracy according to
+    # https://ngc.nvidia.com/catalog/model-scripts/nvidia:resnet_50_v1_5_for_pytorch.
+
+    expansion = 4
+
+    def __init__(self, inplanes, planes, stride=1, downsample=None, groups=1,
+                 base_width=64, dilation=1, norm_layer=None):
+        super(Bottleneck, self).__init__()
+        if norm_layer is None:
+            norm_layer = nn.BatchNorm2d
+        width = int(planes * (base_width / 64.)) * groups
+        # Both self.conv2 and self.downsample layers downsample the input when stride != 1
+        self.conv1 = conv1x1(inplanes, width)
+        self.bn1 = norm_layer(width)
+        self.conv2 = conv3x3(width, width, stride, groups, dilation)
+        self.bn2 = norm_layer(width)
+        self.conv3 = conv1x1(width, planes * self.expansion)
+        self.bn3 = norm_layer(planes * self.expansion)
+        self.relu = nn.ReLU(inplace=True)
+        self.downsample = downsample
+        self.stride = stride
+
+    def forward(self, x):
+        identity = x
+
+        out = self.conv1(x)
+        out = self.bn1(out)
+        out = self.relu(out)
+
+        out = self.conv2(out)
+        out = self.bn2(out)
+        out = self.relu(out)
+
+        out = self.conv3(out)
+        out = self.bn3(out)
+
+        if self.downsample is not None:
+            identity = self.downsample(x)
+
+        out += identity
+        out = self.relu(out)
+
+        return out
+
+
+class ResNet(nn.Module):
+
+    def __init__(self, block, layers, num_classes=7, zero_init_residual=False,
+                 groups=1, width_per_group=64, replace_stride_with_dilation=None,
+                 norm_layer=None):
+        super(ResNet, self).__init__()
+        if norm_layer is None:
+            norm_layer = nn.BatchNorm2d
+        self._norm_layer = norm_layer
+
+        self.inplanes = 64
+        self.dilation = 1
+        if replace_stride_with_dilation is None:
+            replace_stride_with_dilation = [False, False, False]
+        if len(replace_stride_with_dilation) != 3:
+            raise ValueError("replace_stride_with_dilation should be None "
+                             "or a 3-element tuple, got {}".format(replace_stride_with_dilation))
+        self.groups = groups
+        self.base_width = width_per_group
+        self.conv1 = nn.Conv2d(3, self.inplanes, kernel_size=7, stride=2, padding=3,
+                               bias=False)
+        self.bn1 = norm_layer(self.inplanes)
+        self.relu = nn.ReLU(inplace=True)
+        self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
+        self.layer1 = self._make_layer(block, 64, layers[0])
+        self.layer2 = self._make_layer(block, 128, layers[1], stride=2,
+                                       dilate=replace_stride_with_dilation[0])
+        self.layer3 = self._make_layer(block, 256, layers[2], stride=2,
+                                       dilate=replace_stride_with_dilation[1])
+        self.layer4 = self._make_layer(block, 512, layers[3], stride=2,
+                                       dilate=replace_stride_with_dilation[2])    
+        # 10,5,3 59.24
+        # 8,4,2 59.84
+        self.vit_1 = ViT_reduction(image_size=56,patch_size=7,dim=256,channels=256,depth=4,heads=4,mlp_dim=512,dropout=0.3,emb_dropout=0.3)
+        self.vit_2 = ViT_reduction(image_size=28,patch_size=4,dim=512,channels=512,depth=4,heads=4,mlp_dim=1024,dropout=0.3,emb_dropout=0.3)
+        self.vit_3 = ViT_reduction(image_size=14,patch_size=2,dim=1024,channels=1024,depth=4,heads=4,mlp_dim=2048,dropout=0.3,emb_dropout=0.3)
+        
+
+        self.avgpool = nn.AdaptiveAvgPool2d((1, 1))
+        # self.fc_feature = nn.Linear(512*block.expansion, 64)
+        self.fc = nn.Linear(2048, num_classes)
+        
+
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                nn.init.kaiming_normal_(m.weight, mode='fan_out', nonlinearity='relu')
+            elif isinstance(m, (nn.BatchNorm2d, nn.GroupNorm)):
+                nn.init.constant_(m.weight, 1)
+                nn.init.constant_(m.bias, 0)
+
+        # Zero-initialize the last BN in each residual branch,
+        # so that the residual branch starts with zeros, and each residual block behaves like an identity.
+        # This improves the model by 0.2~0.3% according to https://arxiv.org/abs/1706.02677
+        if zero_init_residual:
+            for m in self.modules():
+                if isinstance(m, Bottleneck):
+                    nn.init.constant_(m.bn3.weight, 0)
+                elif isinstance(m, BasicBlock):
+                    nn.init.constant_(m.bn2.weight, 0)
+
+    def _make_layer(self, block, planes, blocks, stride=1, dilate=False):
+        norm_layer = self._norm_layer
+        downsample = None
+        previous_dilation = self.dilation
+        if dilate:
+            self.dilation *= stride
+            stride = 1
+        if stride != 1 or self.inplanes != planes * block.expansion:
+            downsample = nn.Sequential(
+                conv1x1(self.inplanes, planes * block.expansion, stride),
+                norm_layer(planes * block.expansion),)
+
+        layers = []
+        layers.append(block(self.inplanes, planes, stride, downsample, self.groups,
+                            self.base_width, previous_dilation, norm_layer))
+        self.inplanes = planes * block.expansion
+        for _ in range(1, blocks):
+            layers.append(block(self.inplanes, planes, groups=self.groups,
+                                base_width=self.base_width, dilation=self.dilation,
+                                norm_layer=norm_layer))
+
+        return nn.Sequential(*layers)
+
+    def _forward_impl(self, x):
+
+        x = self.conv1(x)
+        x = self.bn1(x)
+        x = self.relu(x)
+        x = self.maxpool(x)
+
+        x = self.layer1(x)
+        y, p_1 = self.vit_1(x)
+        x = x + y
+
+        x = self.layer2(x)
+        y, p_2 = self.vit_2(x)
+        x = x + y
+
+        x = self.layer3(x)
+        y, p_3 = self.vit_3(x)
+        x = x + y
+
+        x = self.layer4(x)
+
+        x = self.avgpool(x)
+        x = torch.flatten(x, 1)
+        x = self.fc(x)
+        x = x + p_1 + p_2 + p_3
+        return x
+
+    def forward(self, x):
+        return self._forward_impl(x)
+
+def resnet_vit():
+    # model = ResNet(BasicBlock, [2, 2, 2, 2])
+    model = ResNet(Bottleneck, [3, 4, 6, 3])
+    return model
